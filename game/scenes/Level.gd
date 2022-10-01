7a14d04ca2b2ffef943d28ea3dd27b4a78bfb08c
Christian Caldwell <christian.caldwell@hotmail.com>
2022-10-01 14:55:47 -0600

    basic script start

--- game/scenes/Level.gd
+++ game/scenes/Level.gd
@@ -10,13 +10,17 @@ var level_tile_map
 
 var game_paused = false
 
-export var = lesser_spawn_creation_timer
-export var = lesser_spawn_timer_max
+export var lesser_spawn_creation_timer : float = 10.0
+export var lesser_spawn_timer_max : float = 17.0 
 
-export var = crew_spawn_curr_count
-export var = crew_spawn_count_max
+export var crew_spawn_curr_count : int = 0
+export var crew_spawn_count_max : int = 10
 
-export var = alert_level
+export var will_countdown : float = 10.0
+var will_timer_max : float = 0
+export var willpower_count : int = 0
+export var willpower_count_max : int = 0
+export var alert_level : int = 0
 
 func _ready() -> void:
 	level_navigation_map = get_world_2d().get_navigation_map()
